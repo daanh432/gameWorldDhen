a0b37dafbbd42d8b1b3f98644d5b2675e94f2aa4
daanh432 <daan@daanhendriks.nl>
2018-11-12 13:20:00 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <meta name="description" content="Game World - Front Page">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>Game World</title>
+</head>
+<body>
+<?php include("menu.html"); ?>
+<div id="frontPageTextDhen">
+    <h1>Welcome to Game World</h1>
+</div>
+</body>
+</html>
