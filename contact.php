f6d05ab50f0f90442a9647d9b5560ed21366e25f
daanh432 <daan@daanhendriks.nl>
2018-11-20 12:50:36 +0100

    Contact page update

--- contact.php
+++ contact.php
@@ -8,40 +8,42 @@
     <title>Game World - Contact</title>
 </head>
 <body>
-<?php include("header.html");
-
-if (!isset($_POST["Send"])) {
-    ?>
-
-    <div id="contactPageFormDhen">
-        <h1>Contact Form Game World</h1>
-        <form class="userFormDhen" method="post" action="contact.php">
-            <label for="name">Your Name:</label>
-            <input type="text" placeholder="Enter your name" name="name">
-            <label for="emailAddress">Your email-address:</label>
-            <input type="email" placeholder="Enter your email address" name="emailAddress">
-            <label for="subject">Message subject:</label>
-            <input type="text" placeholder="Enter the message subject" name="subject">
-            <label for="message">The Message:</label>
-            <input type="text" placeholder="Enter your message" name="message">
-            <label for="Send"></label><input type="submit" name="Send">
-        </form>
-    </div>
+<?php include("header.html"); ?>
+<div class="wrapperDhen">
     <?php
-} else {
-    $msg = $_POST["message"]; // Get the message from POST
-    $subject = $_POST["subject"]; // Get the subject from POST
-    $msg = wordwrap($msg, 80); // New line more then 80 characters on single line
-    mail("daan@daanhendriks.nl", $subject, $msg); // Actually send the email to website administrator
+    if (!isset($_POST["Send"])) {
+        ?>
+        <div id="contactPageFormDhen">
+            <h1>Contact Form Game World</h1>
+            <form class="userFormDhen" method="post" action="contact.php">
+                <label for="name">Your Name:</label>
+                <input type="text" placeholder="Enter your name" name="name">
+                <label for="emailAddress">Your email-address:</label>
+                <input type="email" placeholder="Enter your email address" name="emailAddress">
+                <label for="subject">Message subject:</label>
+                <input type="text" placeholder="Enter the message subject" name="subject">
+                <label for="message">The Message:</label>
+                <input type="text" placeholder="Enter your message" name="message">
+                <label for="Send"></label><input type="submit" name="Send">
+            </form>
+        </div>
+        <?php
+    } else {
+        $msg = $_POST["message"]; // Get the message from POST
+        $name = $_POST["name"];
+        $subject = $_POST["subject"]; // Get the subject from POST
+        $msg = wordwrap($msg, 80); // New line more then 80 characters on single line
+        mail("daan@daanhendriks.nl", "Contact Form Message: " . $subject, "A message from: $name \n\n" . $msg); // Actually send the email to website administrator
+        ?>
+        <div id="contactPageFormDhen">
+            <h1>Thanks for sending us a message!</h1>
+            <p>You'll receive a response in your mailbox!</p>
+            <meta http-equiv="refresh" content="10">
+        </div>
+        <?php
+    }
     ?>
-    <div id="contactPageFormDhen">
-        <h1>Thanks for sending us a message!</h1>
-        <p>You'll receive a response in your mailbox!</p>
-        <meta http-equiv="refresh" content="10">
-    </div>
-    <?php
-}
-
-include("footer.html"); ?>
+</div>
+<?php include("footer.html"); ?>
 </body>
 </html>
