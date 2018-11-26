0243e9f73fe266f58f87e362f4a96758841760be
daanh432 <daan@daanhendriks.nl>
2018-11-26 18:32:19 +0100

    Hotfix: Removed check for returnUrl GET paramater
    Removed this GET parameter a couple of commits ago

--- php/addToBasket.php
+++ php/addToBasket.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-if (isset($_GET["gameId"]) && isset($_GET["returnUrl"])) {
+if (isset($_GET["gameId"])) {
     if (isset($_SESSION["basket"])) {
         if (isset($_SESSION["basket"][$_GET["gameId"]])) {
             $_SESSION["basket"][$_GET["gameId"]] = ["amount" => $_SESSION["basket"][$_GET["gameId"]]["amount"] + 1];
